e3039d87e958a52a91afae4852c6ffd7dc213557
guanren <so.guanren@gmail.com>
2025-05-11 22:39:05 +0800

    feat: add new JSON structures for Tikhub API responses

--- Tikhub.go
+++ Tikhub.go
@@ -2,79 +2,15 @@ package Tikhub
 
 import "github.com/imroc/req/v3"
 
-type xbjson struct {
-	Code   int    `json:"code"`
-	Router string `json:"router"`
-	Params struct {
-		UserAgent    string `json:"user_agent"`
-		RoomID       string `json:"room_id"`
-		UserUniqueID string `json:"user_unique_id"`
-	} `json:"params"`
-	Data struct {
-		XBogus string `json:"x_bogus"`
-	} `json:"data"`
-}
 type Tikhub struct {
 	r              *req.Client
+	Cursor         string
+	Live_cursor    string
+	Internal_ext   string
 	ApiKey         string
 	ua             string
 	room_id        string
 	user_unique_id string
-}
-
-type ttwid struct {
-	Code   int    `json:"code"`
-	Router string `json:"router"`
-	Params struct {
-	} `json:"params"`
-	Data struct {
-		Ttwid string `json:"ttwid"`
-	} `json:"data"`
-}
-type FetchJson struct {
-	Code   int    `json:"code"`
-	Router string `json:"router"`
-	Params struct {
-	} `json:"params"`
-	Data struct {
-		ID                 string `json:"id"`
-		CreateTime         string `json:"create_time"`
-		LastTime           string `json:"last_time"`
-		UserUID            string `json:"user_uid"`
-		UserUIDType        int    `json:"user_uid_type"`
-		FirebaseInstanceID string `json:"firebase_instance_id"`
-		UserAgent          string `json:"user_agent"`
-		BrowserName        string `json:"browser_name"`
-	} `json:"data"`
-}
-type livejson struct {
-	Code   int    `json:"code"`
-	Router string `json:"router"`
-	Params struct {
-		RoomID       string `json:"room_id"`
-		UserUniqueID string `json:"user_unique_id"`
-	} `json:"params"`
-	Data struct {
-		Data []struct {
-			Common struct {
-				Method     string `json:"method"`
-				PlayTime   int    `json:"play_time"`
-				IsShowMsg  bool   `json:"is_show_msg"`
-				RoomID     int64  `json:"room_id"`
-				CreateTime int64  `json:"create_time"`
-				MsgID      int64  `json:"msg_id"`
-			} `json:"common"`
-			Content string `json:"content"`
-		} `json:"data"`
-		Extra struct {
-			Cursor        string `json:"cursor"`
-			LiveCursor    string `json:"live_cursor"`
-			FetchInterval int    `json:"fetch_interval"`
-			Now           int64  `json:"now"`
-		} `json:"extra"`
-		InternalExt string `json:"internal_ext"`
-		StatusCode  int    `json:"status_code"`
-	} `json:"data"`
 }
 
 func NewGithubClient(key string, ua string) *Tikhub {
@@ -96,6 +32,7 @@ func (t Tikhub) Generate_wss_xb_signature() (string, error) {
 	if err != nil {
 		return "", err
 	}
+
 	return x.Data.XBogus, nil
 
 }
@@ -105,7 +42,7 @@ func (t *Tikhub) Fetch_query_user(ttwid string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	f := &FetchJson{}
+	f := &fetchJson{}
 	err = post.UnmarshalJson(f)
 	if err != nil {
 		return "", err
@@ -140,6 +77,9 @@ func (t Tikhub) Fetch_live_im_fetch() error {
 	}
 	t.room_id = l.Params.RoomID
 	t.user_unique_id = l.Params.UserUniqueID
+	t.Internal_ext = l.Data.InternalExt
+	t.Live_cursor = l.Data.Extra.LiveCursor
+	t.Cursor = l.Data.Extra.Cursor
 	// 处理 l.Data
 	return nil
 }
---
+++ struct.go
@@ -0,0 +1,69 @@
+package Tikhub
+
+type xbjson struct {
+	Code   int    `json:"code"`
+	Router string `json:"router"`
+	Params struct {
+		UserAgent    string `json:"user_agent"`
+		RoomID       string `json:"room_id"`
+		UserUniqueID string `json:"user_unique_id"`
+	} `json:"params"`
+	Data struct {
+		XBogus string `json:"x_bogus"`
+	} `json:"data"`
+}
+
+type ttwid struct {
+	Code   int    `json:"code"`
+	Router string `json:"router"`
+	Params struct {
+	} `json:"params"`
+	Data struct {
+		Ttwid string `json:"ttwid"`
+	} `json:"data"`
+}
+type fetchJson struct {
+	Code   int    `json:"code"`
+	Router string `json:"router"`
+	Params struct {
+	} `json:"params"`
+	Data struct {
+		ID                 string `json:"id"`
+		CreateTime         string `json:"create_time"`
+		LastTime           string `json:"last_time"`
+		UserUID            string `json:"user_uid"`
+		UserUIDType        int    `json:"user_uid_type"`
+		FirebaseInstanceID string `json:"firebase_instance_id"`
+		UserAgent          string `json:"user_agent"`
+		BrowserName        string `json:"browser_name"`
+	} `json:"data"`
+}
+type livejson struct {
+	Code   int    `json:"code"`
+	Router string `json:"router"`
+	Params struct {
+		RoomID       string `json:"room_id"`
+		UserUniqueID string `json:"user_unique_id"`
+	} `json:"params"`
+	Data struct {
+		Data []struct {
+			Common struct {
+				Method     string `json:"method"`
+				PlayTime   int    `json:"play_time"`
+				IsShowMsg  bool   `json:"is_show_msg"`
+				RoomID     int64  `json:"room_id"`
+				CreateTime int64  `json:"create_time"`
+				MsgID      int64  `json:"msg_id"`
+			} `json:"common"`
+			Content string `json:"content"`
+		} `json:"data"`
+		Extra struct {
+			Cursor        string `json:"cursor"`
+			LiveCursor    string `json:"live_cursor"`
+			FetchInterval int    `json:"fetch_interval"`
+			Now           int64  `json:"now"`
+		} `json:"extra"`
+		InternalExt string `json:"internal_ext"`
+		StatusCode  int    `json:"status_code"`
+	} `json:"data"`
+}
